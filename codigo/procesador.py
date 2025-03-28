09a2a35f06e0a6c66a538038380d3a58ea6ddbd5
hola0511 <espinosaariassamuel@gmail.com>
2025-03-27 22:32:27 -0500

    mejoras codigo

--- codigo/procesador.py
+++ codigo/procesador.py
@@ -2,7 +2,7 @@ import time
 from control_mensajes import Mensajes
 
 def iniciar_procesamiento(mensajes_obj: Mensajes):
-    print("Procesando mensajes")
+    print("Procesando mensajes.")
     mensajes_obj.clasificacion_de_mensajes()
-    print("Mensajes clasificados y cargados en la cola de prioridad.")
+    print("Mensajes clasificados.")
     time.sleep(1)
